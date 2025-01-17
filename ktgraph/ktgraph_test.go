fe9e588eb5b6b7ae847961f71a21cd3c951ee1c5
Henrique Fürst <hfscheid@gmail.com>
2025-01-16 23:48:42 -0300

    feat: ktgraph parses jsonl and builds basic topology diagrams

--- ktgraph/ktgraph_test.go
+++ ktgraph/ktgraph_test.go
@@ -17,3 +17,15 @@ func TestFromJSON(t *testing.T) {
     t.Log(*g)
   }
 }
+
+func TestDrawing(t *testing.T) {
+  data, err := os.ReadFile("sample.jsonl")
+  if err != nil {
+    t.Fatalf("Could not parse sample file: %v\n", err)
+  }
+  series, err := FromJSON(data)
+  if err != nil {
+    t.Fatalf("Could not parse line: %v\n", err)
+  }
+  series[len(series)-1].DrawPods()
+}
